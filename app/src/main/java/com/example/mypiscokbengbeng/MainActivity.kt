38f53f3069331af7ea790a00406037fffd37f11e
Melia Syuhada <meliasyuhada5@gmail.com>
2021-11-18 15:22:03 +0700

    Publikasi pertama

---
+++ app/src/main/java/com/example/mypiscokbengbeng/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mypiscokbengbeng
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+
+            R.id.btn_move_activity_data -> {
+                val moveIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                startActivity(moveIntent)
+            }
+            }
+        }
+
+    }
---
+++ app/src/main/java/com/example/mypiscokbengbeng/MoveWithDataActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mypiscokbengbeng
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveWithDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_data)
+
+        supportActionBar?.title = "Snack Kripik Sukun"
+    }
+}
